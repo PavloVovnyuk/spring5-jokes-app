b51a6ae4719d2c021be50cdaa74ae04893e57e2c
Pavlo <pawel.wowniuk@gmail.com>
2021-03-07 13:47:26 +0100

    Created Joke Controller. Closes #4

--- src/main/java/pavlo/springframework/spring5jokesapp/services/JokeService.java
+++ src/main/java/pavlo/springframework/spring5jokesapp/services/JokeService.java
@@ -1,5 +1,5 @@
 package pavlo.springframework.spring5jokesapp.services;
 
 public interface JokeService {
-    String getJoek ();
+    String getJoke ();
 }
--- src/main/java/pavlo/springframework/spring5jokesapp/services/JokeServiceImpl.java
+++ src/main/java/pavlo/springframework/spring5jokesapp/services/JokeServiceImpl.java
@@ -13,7 +13,7 @@ public class JokeServiceImpl implements JokeService {
     }
 
     @Override
-    public String getJoek() {
+    public String getJoke() {
         return chuckNorrisQuotes.getRandomQuote();
     }
 }
